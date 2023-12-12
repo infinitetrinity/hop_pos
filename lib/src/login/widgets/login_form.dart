521ca801417e2da9c0881708e10296c93ecfe76c
SyL <yongliang@infinitetrinity.com.sg>
2023-12-12 21:20:54 +0800

    'wip'

--- lib/src/login/widgets/login_form.dart
+++ lib/src/login/widgets/login_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hop_pos/app/app_colors.dart';
+import 'package:hop_pos/app/app_extension.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
@@ -8,12 +10,30 @@ class LoginForm extends StatelessWidget {
     return Expanded(
       flex: 4,
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/logo.png'),
-            const Text('Activate POS License'),
-          ],
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 600),
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Activate POS License',
+                style: context.textTheme.headlineLarge!.copyWith(
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.brand500,
+                ),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Welcome to HOP POS, please sign in with your administrator account and POS license key to begin.',
+                style: context.textTheme.bodyMedium!.copyWith(
+                  color: AppColors.gray900,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
